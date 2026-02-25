4e05164d0267eecffb567453e792532a60e0a34e
Seraj-Abu-Eisha <serajabueisha@gmail.com>
2026-02-25 12:50:46 +0200

    .

---
+++ ViewExapmle/Controllers/HomeController.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace ViewExapmle.Controllers
+{
+    public class HomeController : Controller
+    {
+        [Route("home")]
+        [Route("/")]
+        public IActionResult Index()
+        {
+            return View();
+        }
+    }
+}
---
+++ ViewExapmle/Models/Person.cs
@@ -0,0 +1,8 @@
+﻿namespace ViewExapmle.Models
+{
+    public class Person
+    {
+        public string? name {  get; set; }
+        public DateTime DateOfBirth { get; set; }
+    }
+}
