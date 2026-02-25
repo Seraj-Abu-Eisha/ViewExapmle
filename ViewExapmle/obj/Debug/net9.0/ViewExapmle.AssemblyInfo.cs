399aefc85224250ed82dbf53c62bf071be6a3643
Seraj-Abu-Eisha <serajabueisha@gmail.com>
2026-02-25 12:52:29 +0200

    .

--- ViewExapmle/obj/Debug/net9.0/ViewExapmle.AssemblyInfo.cs
+++ ViewExapmle/obj/Debug/net9.0/ViewExapmle.AssemblyInfo.cs
@@ -14,7 +14,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("ViewExapmle")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+4e05164d0267eecffb567453e792532a60e0a34e")]
 [assembly: System.Reflection.AssemblyProductAttribute("ViewExapmle")]
 [assembly: System.Reflection.AssemblyTitleAttribute("ViewExapmle")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
